926252c08ef1121151553a0fe0bed5e4d94a98bf
L. Packer <53623038+leopackerbt@users.noreply.github.com>
2020-01-22 15:53:08 -0300

    Usado mais um algoritmo: Tempera simulada

--- otimizacao_voos.py
+++ otimizacao_voos.py
@@ -154,7 +154,29 @@ def tempera_simulada(dominio, funcao_custo, temperatura = 10000.0, resfriamento 
     solucao = [random.randint(dominio[i][0], dominio[i][1]) for i in range(len(dominio))]
     
     while temperatura > 0.1:
+        i = random.randint(0, len(dominio) - 1)
+        direcao = random.randint(-passo, passo)
         
+        solucao_temp = solucao[:]
+        solucao_temp[i] += direcao
+        if solucao_temp[i] < dominio[i][0]:
+            solucao_temp[i] = dominio[i][0]
+        elif solucao_temp[i] > dominio[i][1]:
+            solucao_temp[i] = dominio[i][1]
+            
+        custo_solucao = funcao_custo(solucao)
+        custo_solucao_temp = funcao_custo(solucao_temp)
+        probabilidade = pow(math.e, (-custo_solucao_temp - custo_solucao) / temperatura)
+        
+        if (custo_solucao_temp < custo_solucao or random.random() < probabilidade):
+            solucao = solucao_temp
+            
+        temperatura = temperatura * resfriamento
+    return solucao
+
+solucao_tempera_simulada = tempera_simulada(dominio, funcao_custo)
+custo_tempera_simulada = funcao_custo(solucao_tempera_simulada)
+imprimir_agenda(solucao_tempera_simulada)
     
 
 

f3889228e77de2ce0047ac6d82d37994156364d2
L. Packer <53623038+leopackerbt@users.noreply.github.com>
2020-02-13 15:14:49 -0300

    Inicio de criação de uma rede neural utilizando Pybrain

---
+++ exemplo_pybrain.py
@@ -0,0 +1,9 @@
+from pybrain.structure import FeedForwardNetwork
+from pybrain.structure import LinearLayer, SigmoidLayer, BiasUnit
+from pybrain.structure import FullConnection
+
+rede = FeedForwardNetwork()
+
+camadaEntrada = LinearLayer(2)
+camadaOculta = SigmoidLayer(3)
+camadaSaida = SigmoidLayer(1)

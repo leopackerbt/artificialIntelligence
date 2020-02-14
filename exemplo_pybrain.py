adf21afb32655902978f4e66117b4352ea884e9e
L. Packer <53623038+leopackerbt@users.noreply.github.com>
2020-02-14 13:36:44 -0300

    Teste com Pybrain 1

--- exemplo_pybrain.py
+++ exemplo_pybrain.py
@@ -7,3 +7,35 @@ rede = FeedForwardNetwork()
 camadaEntrada = LinearLayer(2)
 camadaOculta = SigmoidLayer(3)
 camadaSaida = SigmoidLayer(1)
+
+#bias camada oculta
+bias1 = BiasUnit()
+
+#bias camada saida
+bias2 = BiasUnit()
+
+# adicionando as camadas dentro da rede (principal)
+rede.addModule(camadaEntrada)
+rede.addModule(camadaOculta)
+rede.addModule(camadaSaida)
+rede.addModule(bias1)
+rede.addModule(bias2)
+
+# ligação entre camada entrada e camada oculta
+entradaOculta = FullConnection(camadaEntrada, camadaOculta)
+ocultaSaida = FullConnection(camadaOculta, camadaSaida)
+
+# ligação bias 1 para camada oculta
+biasOculta = FullConnection(bias1, camadaOculta)
+
+# ligação bias 2 para camada saida
+biasSaida = FullConnection(bias2, camadaSaida)
+
+# a rede irá ser construida aqui
+rede.sortModules()
+
+print(rede)
+print(entradaOculta.params)
+print(ocultaSaida.params)
+print(biasOculta.params)
+print(biasSaida.params)
